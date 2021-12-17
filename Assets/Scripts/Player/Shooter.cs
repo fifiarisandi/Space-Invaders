4e0ec8dda5fa113df192b272f09b523e6604318f
mockba1501 <7790976+mockba1501@users.noreply.github.com>
2021-12-17 17:50:41 +0200

    [Mechanic] Relaunching Rockets
    - Set a timer for relaunching rocket attacks
    - Attached the relaunch timer to the Rocket wait time slider UI component

--- Assets/Scripts/Player/Shooter.cs
+++ Assets/Scripts/Player/Shooter.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 // This script allows the player to shoot projectiles by instantiating them during run-time/gameplay
 public class Shooter : MonoBehaviour
@@ -9,38 +10,60 @@ public class Shooter : MonoBehaviour
     RocketLuncher[] lunchers;
     
     public GameObject projectilePrefab;
+    public Slider roketWaitTimeSlider;
 
     public AudioSource audio;
     public AudioClip bulletSFX;
     public AudioClip rocketSFX;
+
+    public bool rocketFired;
+    public float rocketRelaunchTimer;
+    float launchTime;
 
     // Start is called before the first frame update
     void Start()
     {
         //Get all children function with (s) !!!!
         lunchers = transform.GetComponentsInChildren<RocketLuncher>();
-        //bulletAudio = GetComponent<AudioSource>();
-        //roketAudio = GetComponent<AudioSource>();
+        
+        rocketFired = false;
 
+        roketWaitTimeSlider.maxValue = rocketRelaunchTimer;
+        roketWaitTimeSlider.value = rocketRelaunchTimer;   
     }
 
     // Update is called once per frame
     void Update()
     {
-		// Check if the player pressed the spacebar, mapped to the Jump input in project settings, to make them shoot
+        // Check if the player pressed the spacebar, mapped to the Jump input in project settings, to make them shoot
         if (Input.GetButtonDown("Jump"))
         {
             Shoot();
         }
 
+        
         // Check if the player pressed the 'Left Ctrl' key, mapped to the Jump input in project settings, to make them shoot
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") && !rocketFired)
         {
+            rocketFired = true;
+            launchTime = Time.time;
+            roketWaitTimeSlider.value = 0;
             foreach (var luncher in lunchers)
             {
                 luncher.ShootRocket();
             }
             audio.PlayOneShot(rocketSFX);
+        }
+
+        if (rocketFired)
+        {
+            if(roketWaitTimeSlider.value < rocketRelaunchTimer)
+            {
+                roketWaitTimeSlider.value = Time.time - launchTime;
+
+            }
+            else
+                rocketFired = false;
         }
     }
 
